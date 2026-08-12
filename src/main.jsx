eea046b3a375ec49aaa2684ef8e15d66f50e3f9b
guibatista1193-byte <guibatista1193@gmail.com>
2026-08-12 17:22:35 -0300

    Initialize React application with CadastroAgendamentos

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import CadastroAgendamentos from './CadastroAgendamentos.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <CadastroAgendamentos />
+  </React.StrictMode>
+);
